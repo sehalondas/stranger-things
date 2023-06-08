2eab0773e5a1c6635d89acd6ef4270883933a584
gruntcole <gruntcole1998@gmail.com>
2023-06-07 21:32:55 -0400

    started login component

--- src/client/api.js
+++ src/client/api.js
@@ -1,17 +1,27 @@
 const cohortName = '2303-ftb-et-web-pt';
-const baseUrl = `https://strangers-things.herokuapp.com/api/${cohortName}`;
+const baseUrl = `https://strangers-things.herokuapp.com/api/${cohortName}/posts`;
 
-export const fetchPost = async () => {
+export const fetchPost = async (token) => {
     try {
-        const response = await fetch(`${baseUrl}`);
+        const response = await fetch(`${baseUrl}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer${token}`
+            },
+            body: JSON.stringify({
+                //come back this
+            })
+        });
         const result = await response.json();
         if (result.error) {
             throw result.error;
         }
+        console.log(result);
         return result;
     } catch (error) {
         console.error(error);
     }
 };
 
-//fetchPost()
+fetchPost()
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 import { fetchPost } from "./client/api";
+import {BrowserRouter, route, link} from 'react-router-dom';
 
-const Test =()=>{
-    return <div>Hello</div>
+const Login =()=>{
+    return <div>
+        <form>
+        <h1>Log In</h1>
+        <input type="text" placeholder="Username"></input>
+        </form>
+    </div>
 }
 
-// fetchPost()
+const Main =()=>{
+   return(
+   <BrowserRouter>
+        <Login/>
+   </BrowserRouter>)
+}
 
-ReactDOM.render(<Test/>, document.getElementById('app'))
+ReactDOM.render(<Main/>, document.getElementById('app'))
